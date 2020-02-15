4ebdfb7b4426b052c5d8954910f757a455211b61
SaltyAom <aomkirby@mystiar.com>
2020-02-15 14:09:49 +0700

    :wrench: Remove existed libs, add build size

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     setupFiles: ['<rootDir>/jest.setup.js'],
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     "moduleNameMapper": {
-        "\\.(css|styl|.scss|.less)$": "<rootDir>/__mocks__/styleMock.js"
+        "\\.(css|styl|.scss|.less|.sass)$": "<rootDir>/__mocks__/styleMock.js"
    }
 }
--- libs/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export const isServer = typeof window === "undefined",
-	isEmpty = (obj: Object) =>
-		Object.entries(obj).length === 0 && obj.constructor === Object
